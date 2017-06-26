2599aa62155e3f2b1d5e9c864c9855a227ae9222
WuLiuyan <1254825270@qq.com>
2017-06-26 11:25:17 +0800

    Initial Commit

---
+++ Video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Video
+//
+//  Created by Yong Yang on 2017/6/26.
+//  Copyright © 2017年 wly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Video
+//
+//  Created by Yong Yang on 2017/6/26.
+//  Copyright © 2017年 wly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
